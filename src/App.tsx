3b72a8ec93ac23e3d6e0d0ebc0ee1a33a3e83475
KongJinYong <dimeon@naver.com>
2021-09-06 18:26:59 +0900

    useInput in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import React, { useState } from 'react';
 
+const useInput = (initial: string, validator:Function) => {
+  const [value, setValue] = useState(initial);
+  const onChange = (event :React.ChangeEvent<HTMLInputElement>) => {
+    console.log(typeof event)
+    const { 
+      target: { value }
+    } = event;
+    let willUpdate:boolean = true;
+    
+    willUpdate = validator(value);
+    
+    if (willUpdate){
+      setValue(value);
+      console.log(value);
+    }
+  };
+  return { value, onChange };
+};
+
 
 //function App() {
-  const App = () => {
-  const [item, setItem] = useState(3);
+const App = () => {
+  const maxLen = (value:string) => value.includes("@");
+  const name = useInput("Mr.", maxLen);
+  //  const [item, setItem] = useState(3);
   return (
     <div>
-      HI {item} 
-      <button onClick={() => setItem(item + 1)}>Increment</button>
+      {/* HI {item} 
+       <button onClick={() => setItem(item + 1)}>Increment</button>
       <button onClick={() => setItem(item - 1)}>Decreament</button>
+      <br />
+       */}
+      <input placeholder="Name" {...name} {...onchange} />
     </div>
   );
 }
