e32a290d3f672a86a44b8e35f00b7189e299fb21
KongJinYong <dimeon@naver.com>
2021-09-06 15:51:58 +0900

    useState 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const useInput = () => {
+  
+}
+
+
+//function App() {
+  const App = () => {
+  const [item, setItem] = useState(3);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      HI {item} 
+      <button onClick={() => setItem(item + 1)}>Increment</button>
+      <button onClick={() => setItem(item - 1)}>Decreament</button>
     </div>
   );
 }
