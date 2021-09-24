1dd766abb0947bfa6d9e83149741c06429a004bf
KongJinYong <dimeon@naver.com>
2021-09-24 15:21:26 +0900

    hook practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,70 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import styled, {createGlobalStyle} from "styled-components";
+import axios from 'axios';
 
 
-const useTitle = (initialTitle:string) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle:HTMLTitleElement | null = document.querySelector("title");
-    if (!htmlTitle) return;
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
+
+const Container = styled.div`
+  min-height: 100vh;
+  padding: 200px 0;
+  display: grid;
+  grid-template-columns: repeat(4, 300px);
+  grid-template-rows: repeat(auto-fit, 300px);
+  grid-auto-rows: 300px;
+  grid-gap: 30px 20px;
+  justify-content: center;
+  background: #55efc4;
+  box-sizing: border-box;
+`;
+
+const Post = styled.div`
+  border: 1px solid black;
+  border-radius: 20px;
+  background: white;
+  box-shadow: 10px 5px 5px #7f8fa6;
+`;
+
+const Title = styled.div`
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid black;
+  font-weight: 600;
+`;
+
+const Body = styled.div`
+  height: 80%;
+  padding: 11px;
+  border-radius: 20px;
+`;
+type typePost = {
+  title : string
+  body : string
 }
 
-//function App() {
-const App = () => {
-  const potato:any = useRef();
-  if (potato.current !== null) { return null; }
-  setTimeout(() => (potato.current.focus()), 3000);
+function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(({ data }) => setPosts(data));
+  }, []);
   return (
-    <div>
-    <div>HI</div>
-    <input ref={potato} placeholder="blabla"/>
-    </div>
+    <Container>
+    <GlobalStyle />
+    {posts.map((post: typePost, index) => (
+        <Post key={index}>
+          <Title>{post.title}</Title>
+          <Body>{post.body}</Body>
+        </Post>
+      ))}
+  </Container>
   );
 }
 
